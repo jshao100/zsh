0027ddd4302bace11cf2acce4f4b7833d1b9c1de
Shao, James <JShao@cpg.org>
2015-07-06 16:33:39 -0400

    added clean up to bash script

--- zsh_installer.sh
+++ zsh_installer.sh
@@ -8,11 +8,15 @@ echo `chsh -s /usr/local/bin/zsh`
 echo install oh-my-zsh
 echo `sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"`
 
-echo install fonts
+echo install fonts and then remove folder
 echo `./fonts/install.sh`
+echo `rm -rf fonts/`
 
 echo install syntax highlighting
 echo `cd ~/.oh-my-zsh/custom/plugins`
 echo `git clone git://github.com/zsh-users/zsh-syntax-highlighting.git`
+
+echo remove git repo
+echo `rm -rf .git`
 
 echo installation complete, change font to Menlo in iTerm preferences
